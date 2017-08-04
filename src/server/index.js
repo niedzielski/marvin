b8265c9b7cbbaab613a252e9bdc22ce43324c0f7
joakin <joaquin@chimeces.com>
2017-08-04 11:22:11 +0200

    Add initial server entry point
    ...using express for later compatibility with the service-runner
    
    Adds a / route that responds with Hello world and a fallback handler for
    the rest of urls that sends back 404.

---
+++ src/server/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const { PORT = 3000 } = process.env;
+const server = express();
+
+server.get("/", (req, res) => {
+  res.status(200).send("Hello World!");
+});
+
+server.get("*", (req, res) => {
+  res.status(404).send("Not found");
+});
+
+server.listen(PORT, _ => {
+  console.log(`Server started on port ${PORT}`);
+});
