36f7b5956d3fe0b8a1a26b433364fb61f15f2268
Stephen Niedzielski <stephen@niedzielski.com>
2017-08-07 10:25:54 -0500

    Chore: destrucure Mocha require in page.test

--- test/server/templates/page.test.js
+++ test/server/templates/page.test.js
@@ -1,9 +1,9 @@
 const assert = require("assert");
-const mocha = require("mocha");
+const { describe, it } = require("mocha");
 const page = require("../../../src/server/templates/page");
 
-mocha.describe("page()", () => {
-  mocha.it("incorporeal", () => {
+describe("page()", () => {
+  it("incorporeal", () => {
     const assets = { index: {} };
     assert.ok(page({ assets }));
   });
