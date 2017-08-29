a9fb6084c5c86f1b20868f18e93ad231e4a43ce4
Stephen Niedzielski <stephen@niedzielski.com>
2017-08-29 14:35:09 -0500

    Fix: don't output client files to client dist folder
    - Don't compile and emit files from `src/client` to `dist/client`. This
      was happening for the upcoming router.
    - Limit server and client TypeScript projects to files from their
      subdirectories and common.
    - Move TypeScript definitions to the subdirectory their usage is
      permitted in (client, common, or server).
    
    Change-Id: I33dbad8506b6f82e3874b6d839e70e557acda45f

--- src/server/types/preact-render-to-string.d.ts
+++ src/server/types/preact-render-to-string.d.ts
@@ -1,6 +1,6 @@
 // todo: delete file when
 // https://github.com/developit/preact-render-to-string/pull/39 is merged.
-/// <reference path="../../node_modules/preact/dist/preact.d.ts" />
+/// <reference path="../../../node_modules/preact/dist/preact.d.ts" />
 
 declare namespace render {
   interface Options {
@@ -9,7 +9,11 @@ declare namespace render {
     pretty: boolean;
   }
 
-  function render(vnode: preact.VNode, context?: any, options?: Options): string;
+  function render(
+    vnode: preact.VNode,
+    context?: any,
+    options?: Options
+  ): string;
   function shallowRender(vnode: preact.VNode, context?: any): string;
 }
 
