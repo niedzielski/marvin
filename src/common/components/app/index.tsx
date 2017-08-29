866667f07fb6d14841854219cfd2818f77f56332
Stephen Niedzielski <stephen@niedzielski.com>
2017-08-29 18:57:52 -0500

    Chore: upgrade Prettier to v1.6.1
    A release of Prettier after v1.5.3 breaks semantic versioning. Older
    versions of NPM, such as the version used by CI, ignore the package lock
    file, which is causing master to fail. A response is pending from
    release engineering but no Node.js v8 Jenkins Job Builder templates
    appear to exist. As a workaround, upgrade Prettier to the latest,
    v1.6.1. Highlights include:
    
    - Support for ".prettierignore" files.
    - Support for TypeScript v2.5 RC.
    
    https://github.com/prettier/prettier/releases/tag/1.6.0
    https://github.com/prettier/prettier/releases/tag/1.6.1
    
    Change-Id: If47c23c7053c71622eeb0521b536dbcd3a2a8591

--- src/common/components/app/index.tsx
+++ src/common/components/app/index.tsx
@@ -1,7 +1,8 @@
 import "./index.css";
 import { FunctionalComponent, h } from "preact";
 
-const app: FunctionalComponent<any> = (_props: any): JSX.Element =>
-  <div class="App">Hello world</div>;
+const app: FunctionalComponent<any> = (_props: any): JSX.Element => (
+  <div class="App">Hello world</div>
+);
 
 export default app;
