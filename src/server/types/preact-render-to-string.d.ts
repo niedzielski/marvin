2c9de08d80ba6b7bfd24e125dbaa4ee518808012
Stephen Niedzielski <stephen@niedzielski.com>
2017-09-14 08:28:17 -0500

    Chore: allow TypeScript definitions to be linted
    A follow-up to 04a2349, remove the ESLint ignore entries for TSDs.
    
    Change-Id: I068c52f28e4dae086caab8e188553e248de24963

--- src/server/types/preact-render-to-string.d.ts
+++ src/server/types/preact-render-to-string.d.ts
@@ -1,6 +1,5 @@
 // todo: delete file when
 // https://github.com/developit/preact-render-to-string/pull/39 is merged.
-/// <reference path="../../../node_modules/preact/dist/preact.d.ts" />
 
 declare namespace render {
   interface Options {
