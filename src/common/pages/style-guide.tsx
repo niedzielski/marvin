20ff6ffa2ae585b7a8f03362a4526c9b3634571b
joakin <joaquin@chimeces.com>
2017-09-20 19:52:20 +0200

    Chore: Remove Paper inner spacing
    Let the users of the component decide how they want to space the inside
    themselves.
    
    Additional changes:
    * Add spacing rules to the Page component (header, separator)
      * Remove negative spacing in separator (no need since parts of Page
        have their own inner spacing)
    * Add temporary spacing rules to PageSummary to make it look decent
      until pages/wiki.tsx uses a page template/component with spacing
    * Add a component in the style guide to render the sections with spacing
      * Use inline styles so that dev styles don't end in the
        production CSS for now. May extract a Card component if worth it
        later
    
    Change-Id: I966a995363d2e72b0e76fbc86c930bf67394843b

--- src/common/pages/style-guide.tsx
+++ src/common/pages/style-guide.tsx
@@ -1,27 +1,55 @@
 import { h } from "preact";
 import App from "../components/app/app";
 import Paper from "../components/paper/paper";
+import Separator from "../components/separator/separator";
+import { ChildrenProps } from "../components/preact-utils";
 
 export function Component(): JSX.Element {
   return (
     <App>
-      <Paper>
+      <Card title="Headings">
         <h1>Heading 1</h1>
         <h2>Heading 2</h2>
         <h3>Heading 3</h3>
         <h4>Heading 4</h4>
         <h5>Heading 5</h5>
         <h6>Heading 6</h6>
-      </Paper>
-      <Paper>
+      </Card>
+      <Card title="Text">
         <p>This is a paragraph with paragraph styles.</p>
         <p>This is a second paragraph with paragraph styles.</p>
-      </Paper>
-      <Paper>
+        <Separator />
         <blockquote>
           This is a blockquote with an important sentence.
         </blockquote>
-      </Paper>
+      </Card>
     </App>
+  );
+}
+
+interface CardProps extends ChildrenProps {
+  title: string;
+}
+function Card({ title, children }: CardProps): JSX.Element {
+  /* Use inline styles as we don't want this in the general CSS since it is a
+  dev only route */
+  return (
+    <Paper>
+      <div
+        style={{
+          padding: "var(--space) var(--space) 0"
+        }}
+      >
+        <h5>{title}</h5>
+      </div>
+      <Separator />
+      <div
+        style={{
+          padding: "0 var(--space) var(--space)"
+        }}
+      >
+        {children}
+      </div>
+    </Paper>
   );
 }
