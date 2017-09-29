ef2659d2f7b43b48f489dfca95f1630d3a42e2fa
Stephen Niedzielski <stephen@niedzielski.com>
2017-09-28 21:43:55 -0500

    Chore: move page redirect to distinct type
    This parameter is needed by the upcoming page client.
    
    Bug: T173317
    Change-Id: Ia94cc899d8fe9abac52b0e174a579da810c7dd95

--- src/common/data-clients/page-summary-data-client.ts
+++ src/common/data-clients/page-summary-data-client.ts
@@ -2,24 +2,12 @@ import * as fetch from "isomorphic-unfetch";
 import { PageSummary, PageTitlePath } from "../models/page";
 import { RESTBase } from "../marshallers/restbase";
 import { unmarshalPageSummary } from "../marshallers/page-unmarshaller";
+import { PageRedirect } from "./page-redirect";
 
 // https://en.wikipedia.org/api/rest_v1/#!/Page_content/get_page_summary_title
 export interface Params {
   titlePath: PageTitlePath;
-
-  /**
-   * When enabled (the default), requests for [redirect pages] return an HTTP
-   * 302 with a redirect target in the Location header and content in the body.
-   * When disabled, an HTTP 200 response is returned instead.
-   *
-   * Beware that redirected pre-flighted cross-origin requests (such as those
-   * sending custom request headers like Api-User-Agent) will fail in most
-   * current browsers due to a [spec bug].
-   *
-   * [redirect pages]: https://www.mediawiki.org/wiki/Help:Redirects
-   * [spec bug]: https://github.com/whatwg/fetch/issues/204
-   */
-  redirect?: boolean;
+  redirect?: PageRedirect;
 }
 
 const url = ({ titlePath, redirect }: Params) => {
