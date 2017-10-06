ca39df6482c22709cc30edd4baa809f51c0a9268
Stephen Niedzielski <stephen@niedzielski.com>
2017-10-06 11:18:13 -0500

    Update: remove the menu and search icons
    These buttons are nonfunctional and won't be this quarter. Remove the
    nodes but keep the assets for future usage.
    
    Bug: T177368
    Change-Id: Ia7f7ee86a472d0c68e07228c590742fd654635bd

--- src/common/components/header/header.tsx
+++ src/common/components/header/header.tsx
@@ -1,5 +1,4 @@
 import { h } from "preact";
-import Icon, { menu, search } from "../icon/icon";
 import Wordmark from "../wordmark/wordmark";
 import Link from "../link";
 import { home } from "../../routers/api";
@@ -10,16 +9,13 @@ export default function Header(): JSX.Element {
   return (
     <div className="Header">
       <div className="Header-left">
-        <Icon svg={menu} />
         <Link href={home.toPath()} class="Header-wordmark">
           <Wordmark />
         </Link>
       </div>
       {/* The center placeholder will be used later for desktop sizes */}
       <div className="Header-center" />
-      <div className="Header-right">
-        <Icon svg={search} />
-      </div>
+      <div className="Header-right" />
     </div>
   );
 }
