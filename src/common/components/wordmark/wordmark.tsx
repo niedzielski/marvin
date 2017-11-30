de1e1450bc25a5f401393ab4d8de8aace5d57497
joakin <joaquin@chimeces.com>
2017-11-30 12:20:02 +0100

    Use the production version of the en-wordmark
    Which is smaller (7.8K -> 5.3K) and doesn't use transforms or opacity in
    the SVG, which may be causing blurriness in some devices.
    
    Bug: T181702
    Change-Id: I8c2cfc2dad07a28341d187507f548b560f1199ca

--- src/common/components/wordmark/wordmark.tsx
+++ src/common/components/wordmark/wordmark.tsx
@@ -1,7 +1,13 @@
 import { h } from "preact";
-import "./wordmark.css";
 import { asset } from "../../assets/manifest";
 
 export default function Wordmark(): JSX.Element {
-  return <img className="Wordmark" src={asset("wordmark-en", "svg")} />;
+  return (
+    <img
+      className="Wordmark"
+      src={asset("wordmark-en", "svg")}
+      height="18"
+      width="116"
+    />
+  );
 }
