b7bc3b92fbccc7cd5a82c5d1c1d5098fb7019025
Stephen Niedzielski <stephen@niedzielski.com>
2017-12-07 10:07:36 -0600

    Chore: add router tests for fetch errors
    Add router tests for errors thrown by Route.getInitialProps().
    
    Change-Id: Icc3fc47cd2be000356ff2ac0cd7cc0885c572bc0

--- src/common/router/router.test.ts
+++ src/common/router/router.test.ts
@@ -5,25 +5,38 @@ import * as assert from "assert";
 // a dynamic import)
 // @ts-ignore
 import * as HomeModule from "../pages/home";
-import { RedirectError } from "../http/fetch";
+import { RedirectError, ClientError } from "../http/fetch";
 
 import { newRoute } from "./route";
 import { newRouter } from "./router";
+
+function requestPageModule() {
+  return Promise.resolve({ default: { Component: () => null } });
+}
 
 const routes = [newRoute({ path: "/", page: "home" })];
 
 describe("router()", () => {
   describe(".route()", () => {
-    it("a known route is resolved", () => {
-      return newRouter(routes)
+    it("passes route page name to requestPageModule()", () => {
+      function requestPageModule(page: string) {
+        assert.deepEqual(page, "home");
+        return Promise.resolve({ default: { Component: () => null } });
+      }
+
+      return newRouter(routes, requestPageModule).route("/");
+    });
+
+    it("resolves a known route", () => {
+      return newRouter(routes, requestPageModule)
         .route("/")
         .then(rsp => {
           assert.deepEqual(rsp.status, 200);
         });
     });
 
-    it("an unknown route resolves with the path and appropriate status", () => {
-      return newRouter(routes)
+    it("resolves an unknown route as a 404", () => {
+      return newRouter(routes, requestPageModule)
         .route("/404")
         .then(res => {
           assert.deepEqual(res.status, 404);
@@ -31,8 +44,7 @@ describe("router()", () => {
         });
     });
 
-    // eslint-disable-next-line max-len
-    it("throws redirect errors up for handling on the server/client environment", () => {
+    it("throws redirect errors up for handling by the server or client", () => {
       // Page module that throws a redirect
       const module = {
         default: {
@@ -43,14 +55,9 @@ describe("router()", () => {
         }
       };
 
-      const requestPageModule = (page: string) =>
-        page === "redirect"
-          ? Promise.resolve(module)
-          : Promise.reject(new Error("No page found"));
-
       const routes = [newRoute({ path: "/redirect", page: "redirect" })];
 
-      return newRouter(routes, requestPageModule)
+      return newRouter(routes, () => Promise.resolve(module))
         .route("/redirect")
         .catch(err => {
           assert.ok(
@@ -59,6 +66,48 @@ describe("router()", () => {
           );
           assert.deepEqual(err.status, 301);
           assert.deepEqual(err.url, "/redirected-url");
+        });
+    });
+
+    it("resolves a 404 error in getInitialProps() as a 404 page", () => {
+      const module = {
+        default: {
+          getInitialProps(): Promise<never> {
+            throw new ClientError(404, "/wiki/Nonexistent_title");
+          },
+          Component: () => null
+        }
+      };
+
+      const routes = [newRoute({ path: "/wiki/:title", page: "wiki" })];
+
+      return newRouter(routes, () => Promise.resolve(module))
+        .route("/wiki/Nonexistent_title")
+        .then(rsp => {
+          assert.deepEqual(rsp.status, 404);
+          assert.deepEqual(rsp.props.path, "/wiki/Nonexistent_title");
+        });
+    });
+
+    // eslint-disable-next-line max-len
+    it("resolves an unexpected error in getInitialProps() as an error page and prints an error to stderr", () => {
+      const module = {
+        default: {
+          getInitialProps(): Promise<never> {
+            throw new ClientError(500, "/page/summary/500");
+          },
+          Component: () => null
+        }
+      };
+
+      const routes = [
+        newRoute({ path: "/page/summary/:title", page: "summary" })
+      ];
+
+      return newRouter(routes, () => Promise.resolve(module))
+        .route("/page/summary/500")
+        .then(rsp => {
+          assert.deepEqual(rsp.status, 500);
         });
     });
   });
